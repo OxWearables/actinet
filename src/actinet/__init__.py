6bb11425734df2009cf743762900cf50d08cfd53
Aidan Acquah <abeiku17@gmail.com>
2025-07-10 19:17:39 +0100

    Update classifier version and md5

--- src/actinet/__init__.py
+++ src/actinet/__init__.py
@@ -4,8 +4,8 @@ __maintainer__ = "Shing Chan"
 __maintainer_email__ = "shing.chan@ndph.ox.ac.uk"
 __license__ = "See LICENSE file."
 
-__classifier_version__ = "ssl-ukb-c24-rw-20240308"
-__classifier_md5__ = "b0a345b5d98a179e3deb60625b980796"
+__classifier_version__ = "ssl_ukb_c24_rw_30s_20250710"
+__classifier_md5__ = "53a938a67e11ea91bf88e44382d8597a"
 
 from . import _version
 
