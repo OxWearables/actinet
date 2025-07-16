4495ae6cc66aec4788d2cf72c3615abdf03b49ac
Aidan Acquah <abeiku17@gmail.com>
2025-07-16 16:40:24 +0100

    Retrain and save model

--- src/actinet/__init__.py
+++ src/actinet/__init__.py
@@ -4,8 +4,8 @@ __maintainer__ = "Shing Chan"
 __maintainer_email__ = "shing.chan@ndph.ox.ac.uk"
 __license__ = "See LICENSE file."
 
-__classifier_version__ = "ssl_ukb_c24_rw_30s_20250710"
-__classifier_md5__ = "53a938a67e11ea91bf88e44382d8597a"
+__classifier_version__ = "ssl_ukb_c24_rw_30s_20250716"
+__classifier_md5__ = "7d4cbd5ec5f173fc7babfcf92d46d8bb"
 
 from . import _version
 
