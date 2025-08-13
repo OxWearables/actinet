31b91b677b11dc27315f470492d2aa7930e927f3
Aidan Acquah <abeiku17@gmail.com>
2025-08-13 12:05:18 +0100

    First attempt at adding sleep sedentary transitions to hmm training on c24

--- src/actinet/hmm.py
+++ src/actinet/hmm.py
@@ -16,16 +16,22 @@ class HMM:
         transition=None,
         labels=None,
         uniform_prior=True,
+        handle_transition_gaps=False,
+        handle_sleep_transitions=False
     ):
         self.prior = prior
         self.emission = emission
         self.transition = transition
         self.labels = labels
         self.uniform_prior = uniform_prior
+        self.handle_transition_gaps = handle_transition_gaps
+        self.handle_sleep_transitions = handle_sleep_transitions
 
     def __str__(self):
         return (
             "Hidden Markov Model\n"
+            "Handle transition gaps: {self.handle_transition_gaps}\n"
+            "Correct sleep transitions: {self.handle_sleep_transitions}\n"
             "prior: {prior}\n"
             "emission: {emission}\n"
             "transition: {transition}".format(
@@ -35,7 +41,7 @@ class HMM:
             )
         )
 
-    def fit(self, Y_prob, Y_true, T=None, interval=None):
+    def fit(self, Y_prob, Y_true, groups=None, T=None, interval=None):
         """
         Fit a HMM to the provided data by calculating the prior, transition and emission matrices.
 
@@ -43,6 +49,8 @@ class HMM:
         :type Y_prob: numpy.ndarray
         :param Y_true: Ground truth labels
         :type Y_true: numpy.ndarray
+        :param groups: Group labels for the data, if applicable
+        :type groups: numpy.ndarray, optional
         :param T: Time at each observation
         :type T: numpy.ndarray, optional
         :param interval: Expected time interval between observations in seconds
@@ -58,7 +66,8 @@ class HMM:
             [np.mean(Y_prob[Y_true == label], axis=0) for label in self.labels]
         )
 
-        transition = calculate_transition_matrix(Y_true, T, interval)
+        transition = calculate_transition_matrix(Y_true, groups, T, interval, self.handle_transition_gaps, 
+                                                 self.handle_sleep_transitions)
 
         self.prior = prior
         self.emission = emission
@@ -80,11 +89,13 @@ class HMM:
         :return: Smoothed sequence of activities
         :rtype: np.ndarray
         """
-        check_for_time_values_error(y_obs, t, interval)
+        check_for_input_errors(y_obs, t, interval,
+                               handle_transition_gaps=self.handle_transition_gaps,
+                               handle_sleep_transitions=self.handle_sleep_transitions)
 
         y_smooth = self.viterbi(y_obs, uniform_prior)
 
-        if t is not None:
+        if self.handle_transition_gaps:
             y_smooth = restore_labels_after_gaps(y_obs, y_smooth, t, interval)
 
         return y_smooth
@@ -165,6 +176,8 @@ class HMM:
         self.emission = d["emission"]
         self.transition = d["transition"]
         self.labels = d["labels"]
+        self.handle_transition_gaps = d["handle_transition_gaps"]
+        self.handle_sleep_transitions = d["handle_sleep_transitions"]
 
     def display(self, precision=3):
         """
@@ -176,15 +189,17 @@ class HMM:
         pretty_hmm_params(self, precision=precision)
 
 
-def check_for_time_values_error(Y, T, interval):
+def check_for_input_errors(Y, T, interval, groups=None, handle_transition_gaps=False, handle_sleep_transitions=False):
     # If truthy, T must be the same length as Y, and interval must also be truthy
-    if T is not None:
+    if handle_transition_gaps:
         if len(Y) != len(T):
             raise Exception("Provided times should have same length as labels")
         if not interval:
-            raise Exception(
-                "A window length must be provided when using label times to train hmm"
-            )
+            raise Exception("A window length must be provided when using label times to train hmm")
+
+    if handle_sleep_transitions:
+        if len(Y) != len(groups):
+            raise Exception("Provided group labels should have same length as labels")
 
 
 def restore_labels_after_gaps(y_pred, y_smooth, t, interval):
@@ -203,13 +218,13 @@ def restore_labels_after_gaps(y_pred, y_smooth, t, interval):
     return df["y_smooth"].values
 
 
-def calculate_transition_matrix(Y, t=None, interval=None):
-    # t and interval are used to identify any gaps in the data
-    # If not provided, it is assumed there are no gaps
-    check_for_time_values_error(Y, t, interval)
+def calculate_transition_matrix(Y, groups=None, t=None, interval=None,
+                                 handle_transition_gaps=False, handle_sleep_transitions=False):
 
-    t = t if t is not None else range(len(Y))
-    interval = interval or 1
+    check_for_input_errors(Y, t, interval, groups, handle_transition_gaps, handle_sleep_transitions)
+
+    t = t if handle_transition_gaps else range(len(Y))
+    interval = interval if handle_transition_gaps else 1
 
     df = pd.DataFrame(Y)
 
@@ -221,6 +236,10 @@ def calculate_transition_matrix(Y, t=None, interval=None):
         df = df[(-pd.Series(t).diff(periods=-1) == interval)]
     else:
         df = df[(-pd.Series(t).diff(periods=-1) == pd.Timedelta(seconds=interval))]
+
+    # corret sleep transitions if required
+    if handle_sleep_transitions:
+        df = add_sleep_sedentary_transitions(df, groups)
 
     # add a count column (for group by function)
     df["count"] = 1
@@ -235,6 +254,31 @@ def calculate_transition_matrix(Y, t=None, interval=None):
         raise Exception("No transitions found in data")
 
     return trans_mat
+
+
+def add_sleep_sedentary_transitions(df, groups):
+    # Adds transitions between 1 transition between sleep and sedentary states if it does not exist
+    if groups is None:
+        raise ValueError("Groups must be provided to correct sleep transitions")
+    
+    # Get unique groups
+    unique_groups = groups.unique()
+    for group in unique_groups:
+        group_mask = (groups == group)
+        group_df = df[group_mask]
+
+        # Check if there is at least one sleep and one sedentary state
+        if not ((group_df[0] == 1).any() and (group_df[0] == 2).any()):
+            continue
+
+        # Get the last sleep and first sedentary state
+        last_sleep = group_df[group_df[0] == 1].index.max()
+        first_sedentary = group_df[group_df[0] == 2].index.min()
+
+        # If they are not adjacent, add a transition
+        if last_sleep < first_sedentary:
+            new_row = pd.DataFrame({0: [1], "shift": [2], "count": [1]}, index=[last_sleep + 1])
+            df = pd.concat([df, new_row])
 
 
 def print_array(arr, precision=3):
