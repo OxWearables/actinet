4733419a93c333045351d3b45b9b465429a85ab7
Aidan Acquah <abeiku17@gmail.com>
2025-08-18 14:02:39 +0100

    Final improvements to new model

--- src/actinet/__init__.py
+++ src/actinet/__init__.py
@@ -4,8 +4,8 @@ __maintainer__ = "Shing Chan"
 __maintainer_email__ = "shing.chan@ndph.ox.ac.uk"
 __license__ = "See LICENSE file."
 
-__classifier_version__ = "ssl-ukb-c24-rw-30s-20250717"
-__classifier_md5__ = "b6742740c6bf629bc7a863ebf6e103df"
+__classifier_version__ = "ssl-ukb-c24-rw-30s-20250815"
+__classifier_md5__ = "148a5e798f2465985fe97a3f8b42ba96"
 
 from . import _version
 
